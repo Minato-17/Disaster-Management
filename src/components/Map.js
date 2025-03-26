b44fcce68d61eb72c1c669fd2ad7ddb56e0397c5
Minato <namikazeminato170804@gmail.com>
2025-03-26 14:38:29 +0530

    second commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Map.css';
 
 function Map({ selectedLayer }) {
-  const baseUrl = "https://embed.windy.com/embed2.html";
+  const windyBaseUrl = "https://embed.windy.com/embed2.html";
   const mapConfig = {
     lat: 22.385,
     lon: 77.859,
@@ -23,17 +23,56 @@ function Map({ selectedLayer }) {
     radarRange: -1
   };
 
-  const mapUrl = `${baseUrl}?lat=${mapConfig.lat}&lon=${mapConfig.lon}&detailLat=${mapConfig.detailLat}&detailLon=${mapConfig.detailLon}&width=100%&height=600&zoom=${mapConfig.zoom}&level=${mapConfig.level}&overlay=${selectedLayer}&menu=${mapConfig.menu}&message=${mapConfig.message}&marker=${mapConfig.marker}&calendar=${mapConfig.calendar}&pressure=${mapConfig.pressure}&type=${mapConfig.type}&location=${mapConfig.location}&detail=${mapConfig.detail}&metricWind=${mapConfig.metricWind}&metricTemp=${mapConfig.metricTemp}&radarRange=${mapConfig.radarRange}`;
+  const windyUrl = `${windyBaseUrl}?lat=${mapConfig.lat}&lon=${mapConfig.lon}&detailLat=${mapConfig.detailLat}&detailLon=${mapConfig.detailLon}&width=100%&height=100%&zoom=${mapConfig.zoom}&level=${mapConfig.level}&overlay=${selectedLayer}&menu=${mapConfig.menu}&message=${mapConfig.message}&marker=${mapConfig.marker}&calendar=${mapConfig.calendar}&pressure=${mapConfig.pressure}&type=${mapConfig.type}&location=${mapConfig.location}&detail=${mapConfig.detail}&metricWind=${mapConfig.metricWind}&metricTemp=${mapConfig.metricTemp}&radarRange=${mapConfig.radarRange}`;
+
+  const osmUrl = `https://www.openstreetmap.org/export/embed.html?bbox=${mapConfig.lon - 0.5}%2C${mapConfig.lat - 0.5}%2C${mapConfig.lon + 0.5}%2C${mapConfig.lat + 0.5}&layer=mapnik`;
 
   return (
     <div className="map-container">
-      <iframe
-        width="100%"
-        height="1000"
-        src={mapUrl}
-        frameBorder="0"
-        title="Weather Map"
-      />
+      <div className="windy-map">
+        <iframe
+          width="100%"
+          height="100%"
+          src={windyUrl}
+          frameBorder="0"
+          title="Weather Map"
+        />
+      </div>
+      <div className="partition" />
+      <div className="osm-container">
+        <div className="osm-map">
+          <iframe
+            width="100%"
+            height="100%"
+            src={osmUrl}
+            frameBorder="0"
+            title="OpenStreetMap"
+          />
+        </div>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+                <th>Column 3</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
